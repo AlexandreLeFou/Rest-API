a8af960b41c094e3dca2be6b856228f7792a8be4
AlexandreLeFou <38253545+AlexandreLeFou@users.noreply.github.com>
2018-12-31 20:06:16 +0200

    Update DemoappApplication.java

--- src/main/java/exusapp/demoapp/DemoappApplication.java
+++ src/main/java/exusapp/demoapp/DemoappApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-//@EnableJpaRepositories(basePackages = "exusapp.demoapp.repositories")
 @EnableJpaRepositories("exusapp.demoapp.repositories")
 @SpringBootApplication
 public class DemoappApplication {
